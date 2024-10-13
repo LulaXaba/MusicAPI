d14d0a15481ee331007fbb51083d09ee888e2ef2
Sifund'sihle (Lula) Xaba <95076684+LulaXaba@users.noreply.github.com>
2024-10-13 14:17:43 +0200

    Get playlist by ID: http://localhost:3000/api/playlist/670bb2461371dea443545371

--- routes/Playlist.js
+++ routes/Playlist.js
@@ -17,17 +17,18 @@ router.get('/', auth, async (req, res) => {
 
 // Get a specific playlist by ID
 router.get('/:id', auth, async (req, res) => {
-    try {
-      const playlist = await Playlist.findById(req.params.id).populate('music');
-      if (!playlist) {
-        return res.status(404).json({ message: 'Playlist not found' });
-      }
-      res.json(playlist);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Server Error' });
+  try {
+    const playlist = await Playlist.findById(req.params.id).populate('music');
+    if (!playlist) {
+      return res.status(404).json({ message: 'Playlist not found' });
     }
-  });
+    res.json(playlist);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
 
 // Get all playlists for a user
 router.get('/', auth, async (req, res) => {
